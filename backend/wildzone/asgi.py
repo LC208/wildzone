29c36eee7c7a3d1d299b27568fa43a70b006dbcb
LC208 <maks.2.03@mail.ru>
2026-03-04 22:07:26 +0800

    Инициализация Django проекта

---
+++ backend/wildzone/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for wildzone project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wildzone.settings')
+
+application = get_asgi_application()
